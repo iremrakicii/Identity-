52ee17cd10cd257d47444ae08823fca59c32f98d
iremrakicii <iremrakicii53@gmail.com>
2024-12-30 15:35:23 +0300

    Update User.cs

--- Identity-/Models/User.cs
+++ Identity-/Models/User.cs
@@ -4,14 +4,14 @@ namespace Identity_.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required.")]
         public string PasswordHash { get; set; }
     }
 }
